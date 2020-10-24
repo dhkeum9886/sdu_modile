8d6d63a5bee270e4b61ba48c195d9697d424f75c
dhkeum9886 <dhkeum9886@gmail.com>
2020-10-25 07:44:05 +0900

    * 8주차, 9주차

---
+++ LOTTO/app/src/main/java/com/akj/lotto/AbsoluteActivity.kt
@@ -0,0 +1,12 @@
+package com.akj.lotto
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class AbsoluteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_absolute)
+    }
+}
---
+++ LOTTO/app/src/main/java/com/akj/lotto/RelativeLayoutActivity.kt
@@ -0,0 +1,12 @@
+package com.akj.lotto
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RelativeLayoutActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_relative_layout)
+    }
+}
