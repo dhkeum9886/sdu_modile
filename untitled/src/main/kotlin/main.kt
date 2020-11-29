1bdc1f58328e7d1e94723c2584ad780f9f20ed07
dhkeum9886 <dhkeum9886@gmail.com>
2020-11-29 09:02:21 +0900

    * 13주차 & 과제

---
+++ untitled/src/main/kotlin/main.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+    println("Hello World!")
+//    data class Person(var name: String, var skills : String)
+//    var person = Person("kildong", "kotlin")
+//
+//    var returnObj = person.apply {
+//        this.name = "sean"
+//        this.skills = "java"
+//        "success"
+//    }
+//
+//    println(person)
+//    println("returnObj: $returnObj")
+//
+//    var returnObj2 = person.run {
+//        this.name = "dolly"
+//        this.skills = "c#"
+//        "success"
+//    }
+//
+//    println(person)
+//    println("returnObj2: $returnObj2")
+
+}
